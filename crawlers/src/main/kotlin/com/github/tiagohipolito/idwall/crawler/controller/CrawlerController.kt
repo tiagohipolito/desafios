85524b3d07699734da3e7c8cc0f66835b4f5c3c9
Tiago Hipolito Alves <tiago.alves@miraeducacao.com.br>
2019-03-20 17:17:13 -0300

    rest api reddit crawler

--- crawlers/src/main/kotlin/com/github/tiagohipolito/idwall/crawler/controller/CrawlerController.kt
+++ crawlers/src/main/kotlin/com/github/tiagohipolito/idwall/crawler/controller/CrawlerController.kt
@@ -1,17 +1,15 @@
 package com.github.tiagohipolito.idwall.crawler.controller
 
 import com.github.tiagohipolito.idwall.crawler.service.RedditCrawlerService
-import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.ResponseStatus
+import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class CrawlerController(val redditCrawlerService: RedditCrawlerService) {
 
-    /*@ResponseStatus(HttpStatus.OK)
-    @GetMapping("/upvotes")
-    fun getAll() = redditCrawlerService.get*/
+    @GetMapping("/subreddits/upvotted/{subreddits}")
+    fun getAll(@PathVariable subreddits: String) = redditCrawlerService.retrieveInfo(subreddits).sortedByDescending { it.upvotes }
 
 }
 
